c9ec18c18b88c86305a4f2b34d12605975ff5b75
jeongwoo <dahlia11444@gmail.com>
2021-05-18 16:14:32 +0900

    자바파일 빈등록방법연습

--- src/main/java/com/care/root/config/FileConfig.java
+++ src/main/java/com/care/root/config/FileConfig.java
@@ -1,5 +1,16 @@
 package com.care.root.config;
 
-public class FileConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
+@Configuration //설정 파일
+public class FileConfig {
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver mr = new CommonsMultipartResolver();
+		mr.setMaxUploadSize(52428800);//50MB
+		mr.setDefaultEncoding("utf-8");
+		return mr; //리턴으로 돌려주는 객체가 빈으로 등록
+	}
 }
