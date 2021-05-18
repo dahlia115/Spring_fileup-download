675ed4a97b0c16486970c87a8ddc4ff381433199
jeongwoo <dahlia11444@gmail.com>
2021-05-18 17:25:50 +0900

    파일중복이름 처리

--- src/main/java/com/care/root/service/FileServiceImpl.java
+++ src/main/java/com/care/root/service/FileServiceImpl.java
@@ -2,6 +2,8 @@ package com.care.root.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,9 +14,12 @@ public class FileServiceImpl implements FileService {
 	@Override
 	public void fileProcess(MultipartFile file) {
 		if(file.getSize() != 0) { //파일 사이즈가 0이 아니면(파일 선택)
-		
-			File saveFile = new File(IMAGE_REPO+"\\"+file.getOriginalFilename());
 			
+			SimpleDateFormat sim = new SimpleDateFormat("yyyMMddHHmmss-");
+			Calendar calendar = Calendar.getInstance();//시간얻어오기
+			String sysFileName = sim.format(calendar.getTime())+file.getOriginalFilename();
+			
+			File saveFile = new File(IMAGE_REPO+"\\"+sysFileName);
 			try {
 				file.transferTo(saveFile);
 			} catch (Exception e) {
