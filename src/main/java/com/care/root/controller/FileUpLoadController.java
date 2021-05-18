8a1800e66d984f24570cef6d022fd69c8532747c
jeongwoo <dahlia11444@gmail.com>
2021-05-18 17:19:08 +0900

    파일 image_repo파일에 업로드 확인

--- src/main/java/com/care/root/controller/FileUpLoadController.java
+++ src/main/java/com/care/root/controller/FileUpLoadController.java
@@ -1,13 +1,18 @@
 package com.care.root.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.care.root.service.FileService;
+
 @Controller
 public class FileUpLoadController {
+	@Autowired FileService fs;
+
 	@GetMapping("form")
 	public String form() {
 		return "uploadForm";
@@ -24,6 +29,8 @@ public class FileUpLoadController {
 		System.out.println("id : "+id);
 		System.out.println("name : "+name);
 		System.out.println("originName : "+originName);
+		
+		fs.fileProcess(file);//서비스로 파일 넘기기
 		
 		return "redirect:form";
 	}
--- src/main/java/com/care/root/service/FileServiceImpl.java
+++ src/main/java/com/care/root/service/FileServiceImpl.java
@@ -1,5 +1,8 @@
 package com.care.root.service;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,8 +11,16 @@ public class FileServiceImpl implements FileService {
 
 	@Override
 	public void fileProcess(MultipartFile file) {
-		// TODO Auto-generated method stub
+		if(file.getSize() != 0) { //파일 사이즈가 0이 아니면(파일 선택)
 		
+			File saveFile = new File(IMAGE_REPO+"\\"+file.getOriginalFilename());
+			
+			try {
+				file.transferTo(saveFile);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
