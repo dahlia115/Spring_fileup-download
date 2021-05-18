fd657f066f42aed8dd6b80364f70746c9c8c5b4a
jeongwoo <dahlia11444@gmail.com>
2021-05-18 21:08:06 +0900

    저장된 이미지 출력/파일 다운로드

--- src/main/java/com/care/root/controller/FileDownLoadController.java
+++ src/main/java/com/care/root/controller/FileDownLoadController.java
@@ -1,5 +1,27 @@
 package com.care.root.controller;
 
-public class FileDownLoadController {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.care.root.service.FileService;
+
+@Controller
+public class FileDownLoadController {
+	@GetMapping("download")
+	public void downlowd(@RequestParam("file") String fileName,
+								HttpServletResponse response) throws Exception {
+		response.addHeader("Content-disposition", "attachment; fileName="+fileName);
+		File file = new File(FileService.IMAGE_REPO+"\\"+fileName);
+		FileInputStream in = new FileInputStream(file);
+		FileCopyUtils.copy(in, response.getOutputStream());//왼쪽 값을 오른쪽으로 전달(가져온 파일을 응답하겠다
+		in.close();
+	}
 }
--- src/main/webapp/WEB-INF/views/result.jsp
+++ src/main/webapp/WEB-INF/views/result.jsp
@@ -14,6 +14,12 @@
 		아이디 : ${dto.id }<br>
 		이름 : ${dto.name }<br>
 		이미지 : ${dto.imgName }<br>
+		<img  alt="이미지가 없습니다" width="100px" height="100px" 
+			src="${contextPath }/download?file=${dto.imgName}"><br>
+		<a href="${contextPath }/download?file=${dto.imgName}">
+			${dto.imgName}
+		</a>
+		<hr>
 	</c:forEach>
 	<a href="${contextPath }/form">다시 업로드 하기</a>
 	
