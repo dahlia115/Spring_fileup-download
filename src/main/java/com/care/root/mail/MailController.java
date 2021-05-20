18a08efdb7c045e9ca8a902206aaa2e1c526fc91
jeongwoo <dahlia11444@gmail.com>
2021-05-20 19:48:12 +0900

    이메일 인증test

--- src/main/java/com/care/root/mail/MailController.java
+++ src/main/java/com/care/root/mail/MailController.java
@@ -2,12 +2,15 @@ package com.care.root.mail;
 
 import java.io.PrintWriter;
 
+import javax.mail.Session;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MailController {
@@ -22,4 +25,54 @@ public class MailController {
 		
 		ms.sendMail("dahlia115@naver.com","(제목)테스트 메일","(내용)안녕하세요");//받는 사용자 이메일
 	}
+	@GetMapping("sendmail2")
+	public void sendSipleMail2(HttpServletRequest request,
+						HttpServletResponse response) throws Exception {
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>메일 전송 되었습니다</h1>");
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("<html><body>");
+		sb.append("<h1>제품소개</h1>");
+		sb.append("<a href='https://naver.com'>");
+		sb.append("<img src='https://www.google.co.kr/search?q=%EB%9D%BC%EC%9D%B4%EC%96%B8&sxsrf=ALeKk02kLicbfxxs3LO7KAokc6AyjtJ-7g:1621498614516&tbm=isch&source=iu&ictx=1&fir=0UUAR_znlEG4pM%252C2wg_bGyiwl1LmM%252C_&vet=1&usg=AI4_-kR05t_xrBRx705IlE1g3t6gUiQL2A&sa=X&ved=2ahUKEwig3K-H6dfwAhVGA4gKHSf_DAEQ9QF6BAgQEAE#imgrc=0UUAR_znlEG4pM' whdith='150px' height='150px'>");
+		sb.append("</a></body></html>");
+		
+		String str = sb.toString();
+		ms.sendMail("dahlia115@naver.com","(제목)테스트 메일",str);//받는 사용자 이메일
+	}
+	@GetMapping("auth-form")
+	public String authForm() {
+		return "authentication";
+	}
+	@GetMapping("auth")
+	public String auth(HttpServletRequest request) {
+		ms.auth(request);
+		return "redirect:http://www.naver.com"; 
+	}
+	@GetMapping("auth_check")
+	public String auth_check(@RequestParam String userid,
+							@RequestParam String userkey, HttpSession session) {
+		String sessionKey = (String)session.getAttribute(userid);
+		if(sessionKey != null) {
+			if(sessionKey.equals(userkey)) {
+				session.setAttribute("userid", userid);
+			}
+		}
+		
+		return "redirect:auth-form";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/care/root/mail/MailService.java
+++ src/main/java/com/care/root/mail/MailService.java
@@ -1,6 +1,10 @@
 package com.care.root.mail;
 
+import java.util.Random;
+
 import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -16,11 +20,35 @@ public class MailService {
 			MimeMessageHelper me = new MimeMessageHelper(message,true,"UTF-8");
 			me.setSubject(subject);//제목
 			me.setTo(to);//보내는 곳
-			me.setText(body);//내용
+			me.setText(body, true);//내용 (true로 해야 html문법으로 넘어간다
 			mailSender.send(message);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	public void auth(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		String userid = "test123";
+		String userkey = rand();
+		session.setAttribute(userid, userkey);
+		String body="<h2>안녕하세요</h2>"
+				+"<a href='http://localhost:8085/root/auth_check?"
+				+"userid="+userid+"&userkey="+userkey+"'>인증하기</a>";
+		sendMail("dahlia115@naver.com", "인증(제목)", body);
+	}
+	public String rand() {
+		Random ran = new Random();
+		String str = "";
+		int num;
+		while(str.length() != 20) {
+			num = ran.nextInt(75)+48;//랜덤한 수(0~74)에 +48 ((48~122)아스키 코드값 숫자 ~ 소문자 z
+			if((num >= 48 && num <=57) || (num >= 65 && num <=90) || (num >= 97 && num <= 122)) {
+				
+				str += (char)num;
+				
+			}
+		}
+		return str;
 	}
 }
 
